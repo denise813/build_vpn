6dc3153b8dd08c00323338ac2a509a000d518707
denise813 <denise813@163.com>
2024-01-20 16:32:14 +0800

    coding
    Signed-off-by: denise813 <denise813@163.com>

--- create_vpn.sh
+++ create_vpn.sh
@@ -1,23 +1,45 @@
 net_dev=eth0
 # ppp安装ppp
 yum -y install pptpd
+mkdir -p ./config
+mkdir -p ./origin
 
 vpn_ip=$(ip addr show ${net_dev}|grep inet|grep -v inet6|awk '{split($2, ip, "/"); print ip[1]}')
-cat<< EOF /etc/pptpd.conf
+if [ ! -e origin/pptpd.conf.origin ]; then
+    cp /etc/pptpd.conf ./origin/pptpd.conf.origin
+fi
+file_buff=$(cat /etc/pptpd.conf)
+cat<< EOF >> /etc/pptpd.conf
+${file_buff}
 localip ${vpn_ip}
 remoteip 0.0.0.0
 EOF
 
-cat << EOF /etc/ppp/options.pptpd
-ms-dns 8.8.8.8
-ms-dns 8.8.4.4
+if [ ! -e origin/options.pptpd.origin ]; then
+    cp /etc/ppp/options.pptpd ./origin/options.conf.origin
+fi
+file_buff=$(cat /etc/ppp/options.pptpd)
+cat << EOF >> /etc/ppp/options.pptpd
+${file_buff}
+ms-dns 180.76.76.76
+#ms-dns 180.76.76.76
 EOF
 
-cat << EOF /etc/ppp/chap-secrets
+if [ ! -e origin/chap-secrets.origin ]; then
+    cp /etc/ppp/chap-secrets ./origin/chap-secrets.origin
+fi
+file_buff=$(cat /etc/ppp/chap-secrets)
+cat << EOF >> /etc/ppp/chap-secrets
+${file_buff}
 deise813	hy123456	*
 EOF
 
-cat << EOF /etc/sysctl.conf
+if [ ! -e origin/sysctl.conf ]; then
+    cp /etc/sysctl.conf ./origin/sysctl.conf.origin
+fi
+file_buff=$(cat /etc/sysctl.conf)
+cat << EOF >> /etc/sysctl.conf
+${file_buff}
 net.ipv4.ip_forward = 1
 EOF
 
@@ -29,4 +51,4 @@ systemctl enable pptpd
 
 iptables -nL
 
-route
+#route
